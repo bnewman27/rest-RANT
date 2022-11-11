5666e31bbd51175dbab687c00ec336a3f1c278d7
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2022-11-10 20:45:07 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// DEPENDENCIES
+const { application } = require('express')
+const express = require('express')
+
+//  CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+console.log(PORT)
+
+// ROUTES
+application.get('/', (req, res) => {
+res.send('Welcome to an Awesome App about REST-rants!')
+})
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+})
