9cf1fa171a8d5d5f6f0690d41ae036acf08a29d4
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2022-11-12 15:31:07 -0800

    Adding remaining stub routes

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-// DEPENDENCIES
-const { application } = require('express')
-const express = require('express')
+// // DEPENDENCIES
+// const { application } = require('express')
+// const express = require('express')
 
-//  CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-console.log(PORT)
+// //  CONFIGURATION
+// require('dotenv').config()
+// const PORT = process.env.PORT
+// console.log(PORT)
 
-// ROUTES
-application.get('/', (req, res) => {
-res.send('Welcome to an Awesome App about REST-rants!')
-})
+// // ROUTES
+// application.get('/', (req, res) => {
+// res.send('Welcome to an Awesome App about REST-rants!')
+// })
 
-// LISTEN
-app.listen(PORT, () => {
-    console.log('listening on port', PORT);
-})
+// // LISTEN
+// app.listen(PORT, () => {
+//     console.log('listening on port', PORT);
+// })
