c920b3469eaeb85f445933cbdd3124836cde1971
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2022-11-14 21:19:57 -0800

    Adding Bootstrap grid to index page

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 REST-Rant is an app where users can review restaurants.
  
+ "bom bom" - videogamedunkey
+
+
+<!-- places (restaurants) -->
+
+         name (string), city (string), state (string), cuisines (string) and a pic (string)
--- Views/home.jsx
+++ Views/home.jsx
@@ -3,17 +3,21 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="/images/bowl001.jpg" alt="Veggie and Meat Bowl" />
+                  Photo by <a href="https://unsplash.com/@pwign?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anh Nguyen</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+                
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
     )
-  }
-  
-<a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
-
+}
 
 module.exports = home
+
