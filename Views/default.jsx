b21a7d20338d392954a2a8599d788f0cdcd2b327
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2022-11-25 12:15:40 -0800

    Navbar with style

--- Views/default.jsx
+++ Views/default.jsx
@@ -1,17 +1,37 @@
-const React = require('react')
+const React = require('react');
 
 function Def (html) {
     return (
         <html>
             <head>
                 <title>Title</title>
+                <link 
+                    rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+                    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+                    crossorigin="anonymous"
+                />  
                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">" </link>
             </head>
             <body>
+            <nav>
+                <ul>
+                 <li>
+                  <a href="/">Home</a>
+                 </li>
+                 <li>
+                  <a href="/places">Places</a>
+                 </li>
+                 <li>
+                  <a href="/places/new">Add Place</a>
+                 </li>
+                </ul>
+            </nav>
+
                 {html.children}
             </body>
         </html>
     )
 }
 
-module.exports = Def
+module.exports = Default;
