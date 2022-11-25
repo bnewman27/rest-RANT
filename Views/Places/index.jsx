14daa682b47851067cd65dac0f90f31600d81e9c
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2022-11-25 13:05:29 -0800

    Show route connected

--- Views/Places/index.jsx
+++ Views/Places/index.jsx
@@ -1,59 +1,33 @@
-const React = require('react')
-const Def = require('./default')
-
-
-  
-//   app.get('/', (req, res) => {
-//     let places = [{
-//         name: 'H-Thai-ML',
-//         city: 'Seattle',
-//         state: 'WA',
-//         cuisines: 'Thai, Pan-Asian',
-//         pic: 'Public/images/bowl001.jpg'
-//       }, {
-//         name: 'Coding Cat Cafe',
-//         city: 'Phoenix',
-//         state: 'AZ',
-//         cuisines: 'Coffee, Bakery',
-//         pic: 'Public/images/bowl001.jpg'
-//       }]
-      
-//     res.render('places/index', { places })
-//   })
-
-// module.exports = places
-
+const React = require('react');
+const Default = require('../default');
 function index (data) {
-    console.log(data)
     let placesFormatted = data.places.map((place) => {
-      return (
-        <div className="col-sm-6">
-          <h2>
-          <a href={`/places/${place.id}`}>
-            {place.name}
-          </a>
-        </h2>
-          <p className="text-center">
-            {place.cusinies}
-          </p>
-          <img src={place.pic} alt={place.name} />
-          <p className="text-center">
-            Located in {place.city}, {place.state}
-          </p>
-        </div>
-      )
-    })
+        return (
+            <div className="col-sm-6">
+                <h2>
+                    <a href={`/places/${index}`}>
+                    {place.name}
+                    </a>
+                </h2>
+                <p className="text-left">
+                    {place.cuisines}
+                </p>
+                <img className="img-fluid" src={place.pic} alt={place.name}/>
+                <p className="text-left">
+                    Located in {place.city}, {place.state}
+                </p>
+            </div>
+        );
+    });
     return (
-      <Def>
-          <main>
-              <h1>Places to Rant or Rave About</h1>
-              <div className="row">
-                {placesFormatted}
-              </div>
-          </main>
-      </Def>
-    )
-  }
-  
-
-module.exports = index
+        <Default>
+            <main>
+                <h1>Places to Rant or Rave About</h1>
+                <div className="row">
+                    {placesFormatted}
+                </div>
+            </main>
+        </Default>
+    );
+};
+module.exports = index;
